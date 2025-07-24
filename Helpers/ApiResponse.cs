628723c4afdac8f0c326735b505c4df943ccc027
Arslan Mushtaq <arslanark@gmail.com>
2025-07-25 04:54:51 +0500

    Update ApiResponse to only include data field if data is not null

--- Helpers/ApiResponse.cs
+++ Helpers/ApiResponse.cs
@@ -5,25 +5,27 @@ public static class ApiResponse
     public static IResult Success(object? data = null, string? message = "Success",
         int statusCode = StatusCodes.Status200OK)
     {
-        var response = new
+        var response = new Dictionary<string, object?>()
         {
-            createdAt = DateTime.UtcNow,
-            status = statusCode,
-            message,
-            data
+            ["createdAt"] = DateTime.UtcNow,
+            ["status"] = statusCode,
+            ["message"] = message
         };
+        if (data != null)
+            response["data"] = data;
         return Results.Json(response, statusCode: statusCode);
     }
 
     public static IResult Created(string location, object? data = null, string? message = "Resource Created")
     {
-        var response = new
+        var response = new Dictionary<string, object?>
         {
-            createdAt = DateTime.UtcNow,
-            status = StatusCodes.Status201Created,
-            message,
-            data
+            ["createdAt"] = DateTime.UtcNow,
+            ["status"] = StatusCodes.Status201Created,
+            ["message"] = message,
         };
+        if (data != null)
+            response["data"] = data;
         return Results.Created(location, response);
     }
 }
