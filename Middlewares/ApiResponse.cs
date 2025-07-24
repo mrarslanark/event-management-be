f3c4f9135217575ad6cca02e3671c07984f7d6be
Arslan Mushtaq <arslanark@gmail.com>
2025-07-25 04:20:41 +0500

    Create ApiResponse

---
+++ Middlewares/ApiResponse.cs
@@ -0,0 +1,29 @@
+namespace EventManagement.Middlewares;
+
+public class ApiResponse
+{
+    public static IResult Success(object? data = null, string? message = "Success",
+        int statusCode = StatusCodes.Status200OK)
+    {
+        var response = new
+        {
+            createdAt = DateTime.UtcNow,
+            status = statusCode,
+            message,
+            data
+        };
+        return Results.Json(response, statusCode: statusCode);
+    }
+
+    public static IResult Created(string location, object? data = null, string? message = "Resource Created")
+    {
+        var response = new
+        {
+            createdAt = DateTime.UtcNow,
+            status = StatusCodes.Status201Created,
+            message,
+            data
+        };
+        return Results.Created(location, response);
+    }
+}
